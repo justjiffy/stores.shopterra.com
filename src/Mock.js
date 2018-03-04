0a75df5cb68d08c5bd0f4ca5be68a3fc17de6c04
Juli Emmel <justjiffy@Julis-MacBook-Pro.local>
2018-03-03 18:29:58 -0800

    user can log in

--- src/Mock.js
+++ src/Mock.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
 export const stores = [
   {
     name: "John's Natural",
@@ -12,6 +13,7 @@ export const stores = [
   }
 ]
 
+
 export const users = [
   {
     email: "info@johnsnatural.com",
@@ -22,6 +24,8 @@ export const users = [
     password: "test321"
   }
 ]
+
+
 export class Login extends Component {
   constructor(props) {
     super(props)
@@ -34,51 +38,54 @@ export class Login extends Component {
     }
     this.showLogin = this.showLogin.bind(this)
     this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
+
   handleChange(event, data) {
     event.target.id == "login" ?
       this.setState({ login: event.target.value }) :
       this.setState({ password: event.target.value })
   }
-  showLogin() {
+
+  showLogin(event) {
     this.setState({showForm: true})
   }
 
-  submitLogin() {
-    //submit form
-    //if credentials pass => loadProducts()
-    //else error
+  handleSubmit(event) {
+    event.preventDefault();
+    //  LOOK THROUGH USERS, if user match is found, check password, if password matches, log in
+    //   fetch(baseUrl+"/graphiql", {
+    //     method: "post",
+    //     headers: {
+    //       "Content-Type":"application/json",
+    //       "Accept": 'application/json'
+    //     },
+    //     body: JSON.stringify({ query: `
+    //       {
+              // user {
+              //   login
+              //   password
+              // }
+    //       }
+    //     `})
+    //    })
+    //    .then(res => res.json())
+    //    .then(data => { ...
+    users.map((user) => {
+      console.log(user.email, this.state.login);
+      if(user.email === this.state.login)
+        this.setState({
+          showForm: false,
+          loggedIn: true
+        })
+    });
   }
 
-  // fetchProducts() {
-  //   fetch(baseUrl+"/graphiql", {
-  //     method: "post",
-  //     headers: {
-  //       "Content-Type":"application/json",
-  //       "Accept": 'application/json'
-  //     },
-  //     body: JSON.stringify({ query: `
-  //       {
-  //         products {
-  //           title
-  //           images
-  //           vendor
-  //         }
-  //       }
-  //     `})
-  //    })
-  //    .then(res => res.json())
-  //    .then(data => {
-  //      const products = data.data.products
-  //      this.setState({products})
-  //      return products
-  //    });
-  // }
   render() {
     return (
       <div className="store-login">
         { this.state.showForm ?
-          <form>
+          <form onSubmit={this.handleSubmit}>
             <label>Login: </label>
             <input type="text" id="login" value={this.state.login} onChange={this.handleChange} />
             <label> Password: </label>
@@ -86,6 +93,10 @@ export class Login extends Component {
             <input type="submit" value="submit" />
           </form> :
           <button onClick={this.showLogin}>Store Login</button>
+        }
+        { this.state.loggedIn ?
+          <h3>Logged In!</h3> :
+          <h3></h3>
         }
       </div>
     );
