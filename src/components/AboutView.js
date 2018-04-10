e02bc1f81f63e90217e31075dbb52b43a569306a
Your Name <justjiffy@gmail.com>
2018-04-10 14:35:33 -0700

    updated all desktol desktop and mobile styles and assets

--- src/components/AboutView.js
+++ src/components/AboutView.js
@@ -11,7 +11,7 @@ export default class AboutView extends Component {
         <h1>{"Natural, sustainable living and peace of mind coming together in harmony"}</h1>
         <h3>{"Sell your product to customers that care about how it impacts them and the environment."}</h3>
         <Signup pitch="Get notified when we launch" type="preLaunch" />
-        <section className="About">
+        <section className="About margin">
           <Intro />
           <Rating />
           <Example />
@@ -19,7 +19,7 @@ export default class AboutView extends Component {
         <Why />
         <Categories />
         <div className="signup">
-          <Signup pitch="Be the first to sell on Shopterra" type="preLaunch" />
+          <Signup pitch="Get notified when we launch" type="preLaunch" />
         </div>
       </div>
     )
@@ -32,7 +32,10 @@ class Rating extends Component {
       <div className="Rating">
         <h2>Shopterra Product Rating System</h2>
         <p>{"Simple. Transparent. Independent."}</p>
-        <GoodForYou />
+        <div className="good-container">
+          <GoodForYou />
+          <GoodForEarth />
+        </div>
       </div>
     )
   }
@@ -47,19 +50,19 @@ class Intro extends Component {
         <div className="icons">
           <div>
             <img src={images["value1.png"]} alt="share" />
-            <span>Share Your Values</span>
+            <p>Share Your Values</p>
           </div>
           <div>
             <img src={images["value2.png"]} alt="reach" />
-            <span>Reach a Wider Audience</span>
+            <p>Reach a Wider Audience</p>
           </div>
           <div>
             <img src={images["value3.png"]} alt="increase" />
-            <span>Increase Your Sales</span>
+            <p>Increase Your Sales</p>
           </div>
           <div>
             <img src={images["value4.png"]} alt="save" />
-            <span>Save Time & Mone</span>
+            <p>Save Time & Money</p>
           </div>
         </div>
       </div>
@@ -70,43 +73,64 @@ class Intro extends Component {
 class GoodForYou extends Component {
   render() {
     return (
-      <div className="GoodFor">
+      <div className="GoodFor margin-fix">
         <img className="rating" src={images["icon_you.png"]} alt="rating" />
         <h2 className="pink">Good For You</h2>
-        <p>Some of the factors we consider:</p>
+        <p>{'Our “Good for You” rating reflects how safe a product is for use by you and your family, and how naturally-derived the ingredients are. A higher “Good for You” rating indicates that a product has undergone accredited safety testing and does not contain ingredients that are known to cause chronic or acute health effects. A higher rating also indicates a higher percentage of naturally-derived ingredients within the product. Products with a low “Good for You” rating will not be offered on Shopterra.'}</p>
           <ul>
-            <li>rating factor</li>
-            <li>rating factor</li>
-            <li>rating factor</li>
-            <li>rating factor</li>
-            <li>rating factor</li>
-            <li>rating factor</li>
-            <li>rating factor</li>
-            <li>rating factor</li>
+            <li>Certified Safety Testing</li>
+            <li>Naturally-derived or synthetic</li>
+            <li>Ingredients</li>
+            <li>Parabin-free</li>
+            <li>CIT/MIT-free</li>
+            <li>Free from formaldehyde donors</li>
+            <li>Sulfate-free</li>
+            <li>Glycol-free</li>
+            <li>Alcohol-free</li>
+            <li>Silicone-free</li>
+            <li>Free of synthetic dyes and fragrances</li>
           </ul>
+          <hr className="mobile"></hr>
       </div>
     )
   }
 }
 
+class GoodForEarth extends Component {
+  render() {
+    return (
+      <div className="GoodFor">
+        <img className="rating" src={images["icon_earth.png"]} alt="rating" />
+        <h2 className="green">Kind to Earth</h2>
+        <p>{'Our “Kind to Earth” rating reflects the degree to which a product and packaging affects the environment during use and disposal. A higher “Kind to Earth” rating indicates that the product and packaging are highly biodegradable, recyclable, and leaves a lower carbon footprint impact on the environment. Products with a low “Kind to Earth” rating will not be offered on Shopterra.'}</p>
+          <ul>
+            <li>Product impact to the environment</li>
+            <li>Product biodegradability</li>
+            <li>Product carbon footprint</li>
+            <li>Packaing impact to the environment</li>
+            <li>Packaging ease of re-use</li>
+            <li>Recyclability</li>
+          </ul>
+          <hr className="mobile"></hr>
+      </div>
+    )
+  }
+}
 
 class Example extends Component {
   render() {
     return (
       <div className="Example">
-        <div className="heading">
-          <p className="mobile">Example Ratings</p>
-          <p className="desktop">{"You’ll see this rating on all products sold on Shopterra."}</p>
+        <div className="heading desktop">
+          {"You’ll see this rating on all products sold on Shopterra."}
         </div>
         <div className="content-container">
           <div className="content">
-            <img src={images["example_product.jpg"]} alt="example" />
-            <hr className="mobile"></hr>
+            <img src={images["example_product.png"]} alt="example" />
           </div>
           <div className="content">
-            <p className="patent">Blurb about the patent pending system, and your cooperation with other agencies as well as Eshe’s credentials.</p>
-            <img className="uspto" src={images["agency_uspto.png"]} alt="uspto" />
-            <img className="fsa" src={images["agency_fsa.png"]} alt="fsa" />
+            <p className="patent">{"Shopterra’s unique product rating system developed through extensive industry experience, and verified by the highly respected NSF International, assesses safety and source of ingredients, and the environmental impact of all the products we bring to you."}</p>
+            <img className="nsf" src={images["agency_nsf.png"]} alt="fsa" />
           </div>
         </div>
       </div>
@@ -120,8 +144,7 @@ class Why extends Component {
       <div className="Why">
         <h1>Why Sell On Shopterra?</h1>
         <h3>Grow your customer base.</h3>
-        <p className="mobile">A small paragraph outlining the ethos behind the company. A high level description of the problem you want to solve for customers.</p>
-        <p className="desktop">Shopterra offers one platform for natural and environmentally conscious consumers to purchase products with confidence. Our proprietary rating system ranks active ingredients in each and every product we sell and tells you, the consumer, how it impacts your health and the environment.</p>
+        <p>Shopterra offers one platform for both natural and environmentally conscious consumers to purchase products with confidence. Our simple, transparent rating system, developed independently through extensive experience and verified by NSF International, increases trust in your products and differentiates your brand from conventional products that are often neither natural nor environmentally safe. We are building a community of like-minded consumers who share your values of living a natural lifestyle and we help them inspire their friends and families to choose natural, sustainable products like yours to make a difference in their lives and for our planet, so you can reach a wider audience online.</p>
       </div>
     )
   }
@@ -137,13 +160,13 @@ class Categories extends Component {
             <img src={images["category_personalcare.jpg"]}  alt="category"/>
           </div>
           <div>
-            <img src={images["category_nutriments.jpg"]} alt="category"/>
+            <img src={images["category_petcare.jpg"]} alt="category"/>
           </div>
           <div>
             <img src={images["category_homecare.png"]} alt="category"/>
           </div>
           <div>
-            <img src={images["category_petcare.jpg"]} alt="category"/>
+            <img src={images["category_nutriments.jpg"]} alt="category"/>
           </div>
         </div>
       </div>
